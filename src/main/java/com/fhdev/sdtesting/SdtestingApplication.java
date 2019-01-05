eff266261e09e731d127e0d500eec604bd8f1e5f
Fanuel Pereira <riquezerofive@gmail.com>
2019-01-05 18:47:16 -0200

    Projeto criado - Classes Assunto e Questao

---
+++ src/main/java/com/fhdev/sdtesting/SdtestingApplication.java
@@ -0,0 +1,14 @@
+package com.fhdev.sdtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdtestingApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/fhdev/sdtesting/domain/Questao.java
@@ -0,0 +1,85 @@
+package com.fhdev.sdtesting.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Questao implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+	
+	@ManyToOne
+	@JoinColumn(name = "assunto_id")
+	private Assunto assunto;
+	
+	public Questao() {
+		
+	}
+	
+	public Questao(Integer id, String descricao, Assunto assunto) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.assunto = assunto;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Assunto getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(Assunto assunto) {
+		this.assunto = assunto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Questao other = (Questao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
